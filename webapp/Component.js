51290942052b7d7dacae620f45c9b08480f0d053
Jacquesssan <wozik@centrum.cz>
2020-07-19 11:19:33 +0200

    finis

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,31 +1,41 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-	"./controller/HelloDialog"
-], function (UIComponent, JSONModel, HelloDialog) {
+	"./controller/HelloDialog",
+	"sap/ui/Device"
+], function (UIComponent, JSONModel, HelloDialog, Device) {
 	"use strict";
 
 	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
 
-		metadata : {
+		metadata: {
 			manifest: "json"
 		},
 
-		init : function () {
+		init: function () {
 			// call the init function of the parent
 			UIComponent.prototype.init.apply(this, arguments);
 
 			// set data model
 			var oData = {
-				recipient : {
-					name : "World"
+				recipient: {
+					name: "World"
 				}
 			};
 			var oModel = new JSONModel(oData);
 			this.setModel(oModel);
 
+			// set device model
+			var oDeviceModel = new JSONModel(Device);
+			oDeviceModel.setDefaultBindingMode("OneWay");
+			this.setModel(oDeviceModel, "device");
+
 			// set dialog
 			this._helloDialog = new HelloDialog(this.getRootControl());
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+
 		},
 
 		exit : function () {
@@ -35,6 +45,17 @@ sap.ui.define([
 
 		openHelloDialog : function () {
 			this._helloDialog.open();
+		},
+
+		getContentDensityClass : function () {
+			if (!this._sContentDensityClass) {
+				if (!Device.support.touch) {
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
 		}
 
 	});
